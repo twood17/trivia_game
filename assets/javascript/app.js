7ad2d473ddfc7615e9a5b82246528fbad898619b
Ty Wood <twood06@gmail.com>
2019-03-13 20:11:58 -0400

    JS beginning outline

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+// timer for time to answer questions
+// multiple choice questions [array?]
+// questions chosen from a for(?) loop
+// answers assigned to questions
+// question answered in time
+// number right/wrong
+// display correct answer
